f57659358cc2bb52a2640d5c7c20ebd788cc5dea
staniszmatt <matthewpstaniszewski@gmail.com>
2019-02-09 10:50:28 -0800

    react-intro - Matthew Staniszewski - Completed prototype 1 of 3.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+let user = {
+   name: "Matt",
+   luckyNumber: luckyNumber()
+}
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
-    document.getElementById('root')
+   greeting(user), 
+   document.getElementById('root')
 );
+
+function luckyNumber() {
+	return Math.floor(Math.random() * (1000 - 1)) + 1;
+}
+
+function greeting(displayInfo) {
+   return (
+      <div className="container">
+         <h1 className="user">Welcome {displayInfo.name}.</h1>
+         <h2 className="text-muted">Your lucky number is {displayInfo.luckyNumber}</h2>
+      </div>
+   )
+}
+
