f90db83c3153e88a153c2ef81faaaf53c91fb0f1
Matthew Staniszewski <34016859+staniszmatt@users.noreply.github.com>
2019-02-09 12:36:00 -0800

    Revert "Functional components - Merging second part of react prototype"

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
 
-let user = {
-   name: "Matt",
-   luckyNumber: luckyNumber()
-}
 ReactDOM.render(
-   <App/>, 
-   document.getElementById('root')
+    <h1 className="container">React Prototypes!</h1>,
+    document.getElementById('root')
 );
-
-function luckyNumber() {
-	return Math.floor(Math.random() * (1000 - 1)) + 1;
-}
-
-function greeting(displayInfo) {
-   return (
-      <div className="container">
-         <h1 className="user">Welcome {displayInfo.name}.</h1>
-         <h2 className="text-muted">Your lucky number is {displayInfo.luckyNumber}</h2>
-      </div>
-   )
-}
-
