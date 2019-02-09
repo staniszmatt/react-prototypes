9032f2328917cbfde724c954a81bbc27df1cfdba
staniszmatt <matthewpstaniszewski@gmail.com>
2019-02-09 13:33:07 -0800

    Added start and stop buttons and methodes. onClick binded to constructor

--- src/components/stopwatch.js
+++ src/components/stopwatch.js
@@ -8,15 +8,31 @@ class Stopwatch extends Component{
          start: null,
          elapsed: 0
       }
+      this.start = this.start.bind(this);
+      this.stop = this.stop.bind(this);
+   }
+   start(){
+      this.setState({
+         status: "running",
+         start: new Date().getTime()
+      });
+   }
+   stop(){
+      this.setState({
+         status: "stopped",
+      });
    }
 
    render(){
       const {status, elapsed} = this.state;
+
       return(
          <div>
             <h1>
                {elapsed}
                <p>{status}</p>
+               <button onClick={this.start}>Start</button>
+               <button onClick={this.stop}>Stop</button>
             </h1>
          </div>
       );
