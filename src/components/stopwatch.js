58a5fa2bd6ccafb918355d498db1f7d19002ff9e
staniszmatt <matthewpstaniszewski@gmail.com>
2019-02-09 14:43:23 -0800

    updated start, stop and reset with functionality

--- src/components/stopwatch.js
+++ src/components/stopwatch.js
@@ -7,23 +7,43 @@ class Stopwatch extends Component{
          status: "stopped",
          start: null,
          elapsed: 0
-      }
+      };
       this.start = this.start.bind(this);
       this.stop = this.stop.bind(this);
+      this.update = this.update.bind(this);
+      this.reset = this.reset.bind(this);
+   }
+   update(){
+      let {status, start} = this.state;
+      if (status === "running"){
+         this.setState({
+            elapsed: new Date().getTime() - start
+         });
+         setTimeout(this.update, 10);
+      }
    }
    start(){
       this.setState({
          status: "running",
          start: new Date().getTime()
       });
+      setTimeout(this.update, 10);
    }
    stop(){
       this.setState({
-         status: "stopped",
+         status: "stopped"
       });
+   }
+   reset(){
+      this.setState({
+         status: "stopped",
+         start: null,
+         elapsed: 0
+      })
    }
 
    render(){
+      
       const {status, elapsed} = this.state;
 
       return(
@@ -33,6 +53,7 @@ class Stopwatch extends Component{
                <p>{status}</p>
                <button onClick={this.start}>Start</button>
                <button onClick={this.stop}>Stop</button>
+               <button onClick={this.reset}>Reset</button>
             </h1>
          </div>
       );
