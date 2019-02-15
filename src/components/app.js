3aa7839ca8cb1d61030e4c0aaa9f383f8b1e1ea8
staniszmatt <matthewpstaniszewski@gmail.com>
2019-02-15 08:53:09 -0800

    Feature Set 2 - Create welcome page and added Router to index and app files

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Welcome from '../components/welcome';
+import {Route} from 'react-router-dom';
+   
 
 export default () => {
    return (
    <div className="container">
       <h1>This is ROUTING!</h1>
+      <Route path="/" component={Welcome}/>
    </div>
    )
 }
