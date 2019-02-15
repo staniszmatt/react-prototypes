0639dd287cec79ccfb4ae8c024972bacca770783
staniszmatt <matthewpstaniszewski@gmail.com>
2019-02-15 09:18:19 -0800

    Added Macarons page and linked it. Exact path for welcome screen and now dipsplay both pages seperatly.

--- src/components/app.js
+++ src/components/app.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Welcome from '../components/welcome';
 import {Route} from 'react-router-dom';
 import Nav from '../components/nav';
-
-   
+import DisplayMacarons from '../components/our_macarons'
 
 export default () => {
    return (
    <div className="container">
       <Nav/>
-      <Route path="/" component={Welcome}/>
+      <Route exact path="/" component={Welcome}/>
+      <Route path='/our_macarons' component={DisplayMacarons}/>
    </div>
    )
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+/**
+ * 
+ *@param props - an empty object but needed for router to work. 
+ */
 export default (props) => {
    return (
       <ul className="nav nav-tabs mt-3">
          <li className="nav-item">
             <Link to="/" className="nav-link" >Welcome!</Link>
+         </li>
+         <li className="nav-item">
+            <Link to="/our_macarons" className="nav-link" >Macarons!</Link>
          </li>
       </ul>
    )
